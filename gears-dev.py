cd019fd6bf79cd7e9132507cec17998a63b3fe34
Jürgen Weigert <juewei@fabfolk.com>
2014-03-20 16:52:00 +0100

    Added experimental setup.py Not sure if this can install into inkex_dir or not.

--- gears-dev.py
+++ gears-dev.py
@@ -1,7 +1,10 @@
 #! /usr/bin/env python
+# -*- coding: utf-8 -*-
 '''
 Copyright (C) 2007 Aaron Spike  (aaron @ ekips.org)
 Copyright (C) 2007 Tavmjong Bah (tavmjong @ free.fr)
+Copyright (C) http://cnc-club.ru/forum/viewtopic.php?f=33&t=434&p=2594#p2500
+Copyright (C) 2014 Jürgen Weigert (juewei@fabfolk.com)
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
---
+++ setup.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# 
+# sudo zypper in python-setuptools
+# http://docs.python.org/2/distutils/setupscript.html#installing-additional-files
+#
+import sys,os,glob,re
+
+from distutils.core import setup
+from setuptools.command.test import test as TestCommand
+
+inkex_dir='/usr/share/inkscape/extensions'
+sys.path.append(inkex_dir)        # gears-dev wants to import inkex.py
+import importlib	
+g = importlib.import_module('gears-dev')        # import 'gears-dev' as g
+
+## Unfinished.
+
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+    def run_tests(self):
+        #import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.test_args)
+        sys.exit(errno)
+
+
+
+setup(name='inkscape-gears-dev',
+      version = g.__version__,
+      description="Inkscape extension for generating gears. Development version.",
+      author="Jürgen Weigert, et.al.",
+      author_email="juewei@fabfolk.com",
+      url='https://github.com/jnweiger/inkscape-gears-dev',
+      scripts=['gears-dev.py', 'gears-dev.inx', 'README.md'],
+      license='GPL-2.0',
+      classifiers=[
+          'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+          'Environment :: Console',
+          'Development Status :: 5 - Production/Stable',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+                  ],
+      cmdclass={'test': PyTest},
+      long_description="".join(open('README.md').readlines()),
+      # tests_require=['pytest'],
+      #packages=['inkscape-extensions-extra' ],
+      install_dir=inkex_dir,
+# 
+)
